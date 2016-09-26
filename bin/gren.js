8eab41efa222753f740cc88665ea316f613598ba
alexcanessa <canessa.alex@gmail.com>
2016-09-26 16:31:00 +0100

    Create a bin/gren.js for the global usage

---
+++ bin/gren.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+ 
+'use strict';
+
+var GithubReleaseNotes = require('../src/gren');
+var gren = new GithubReleaseNotes();
+var utils = require('../src/utils');
+
+var action = utils.getBashOptions(process.argv)['action'];
+
+gren.init()
+	.then(function (success) {
+		if(success) {
+			return gren[action || 'release']();
+		}
+	});
