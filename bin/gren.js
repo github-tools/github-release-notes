eaeeecc5a5af0a77b333c8454f63e4dfa9c1315c
alexcanessa <canessa.alex@gmail.com>
2017-03-09 17:29:21 +0000

    Require github-release-notes.js in the bin file

--- bin/gren.js
+++ bin/gren.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
 
-'use strict';
-
-var GithubReleaseNotes = require('./src/gren');
-var gren = new GithubReleaseNotes();
-var utils = require('./src/utils');
-var action = utils.getBashOptions(process.argv)['action'];
-
-gren.init()
-    .then(function() {
-        return gren[action || 'release']();
-    });
+require('../github-release-notes');
