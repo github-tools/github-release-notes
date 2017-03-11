67e38cb578f754aa18648c55932eb2b5801909ea
Alex Canessa <canessa.alex@gmail.com>
2017-03-11 23:10:01 +0000

    Generate docs (#42)
    * Add docs site
    
    * Remove .sass-cache
    
    * Remove old files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     grunt.initConfig({
@@ -17,21 +16,8 @@ module.exports = function(grunt) {
                 'Gruntfile.js',
                 'src/**/*.js'
             ]
-        },
-        jsdoc: {
-            dist: {
-                src: ['README.md', 'src/*.js'],
-                readme: 'README.md',
-                version: true,
-                options: {
-                    destination: 'docs',
-                    template: 'node_modules/ink-docstrap/template',
-                    configure: 'node_modules/ink-docstrap/template/jsdoc.conf.json'
-                }
-            }
         }
     });
 
     grunt.registerTask('test', ['eslint', 'nodeunit']);
-    grunt.registerTask('ship', ['test', 'jsdoc']);
 };
