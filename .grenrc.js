0b44811e82f8e92e03402100e44eae27447cb365
alexcanessa <canessa.alex@gmail.com>
2017-03-16 12:03:28 +0000

    Change gren config file for output of issues

--- .grenrc.js
+++ .grenrc.js
@@ -4,5 +4,13 @@ module.exports = {
         "wontfix",
         "invalid",
         "help wanted"
-    ]
+    ],
+    "template": {
+        issue: ({ text, name, url, labels }) => {
+            labels = labels.slice(0, -1);
+
+            return `- [${text}](${url}) ${name} - ${labels}`;
+        },
+        "label": "_{{label}}_,"
+    }
 }
