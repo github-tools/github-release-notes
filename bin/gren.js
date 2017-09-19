98f9543663508e2e463d0bcfbfa23e3f374dbc2f
alexcanessa <canessa.alex@gmail.com>
2017-09-20 00:39:05 +0100

    Add binary files generated via gulp

--- bin/gren.js
+++ bin/gren.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
+'use strict';
 
-require('../github-release-notes');
+var _commander = require('commander');
+
+var _commander2 = _interopRequireDefault(_commander);
+
+var _package = require('../package.json');
+
+var _utils = require('../dest/utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_commander2.default.version(_package.version).description(_package.description).usage('[command] [options]').command('release', 'Release into chunk').alias('r').command('changelog', 'Write a motherfucking changelog').alias('c').on('--help', function () {
+    console.log('');
+    console.log('  Examples:');
+    console.log('');
+    console.log('    $ gren release');
+    console.log('');
+    console.log('    $ gren help release');
+    console.log('');
+}).parse((0, _utils.argvWithVersion)(process.argv));
